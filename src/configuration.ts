0675f85c32ca9d067ec84e17fa0c5bf34b406c57
TGrochowski87 <t.grochowski99@gmail.com>
2023-12-08 14:59:46 +0100

    longer lifespan

--- src/configuration.ts
+++ src/configuration.ts
@@ -25,7 +25,7 @@ export const sensorRayCount: number = 8;
 export const populationSize: number = 200;
 export const weightMutationRate: number = 0.1;
 export const weightPerturbationChance: number = 0.9;
-export const generationLifetime: number = 2000;
+export const generationLifetime: number = 3000;
 
 // Machine Learning NEAT
 export const compatibilityCoefficients: [number, number, number] = [1.0, 1.0, 0.3];

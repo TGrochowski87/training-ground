e022ea0162a8d2fabda8453fd77855c8de719f2a
TGrochowski87 <t.grochowski99@gmail.com>
2023-12-19 22:30:15 +0100

    game mode fix

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,4 @@
-import { enemySpawnPoint, gameScreenHeight, gameScreenWidth } from "configuration";
+import { enemySpawnPoint, gameScreenHeight, gameScreenWidth, sites } from "configuration";
 import Enemy from "entities/enemy";
 import Player from "entities/player";
 import WallCollection from "entities/wallCollection";
@@ -7,6 +7,7 @@ import NeuralNetworkConventional from "machine-learning/conventional/neuralNetwo
 import EnemyNEAT from "machine-learning/NEAT/enemyNEAT";
 import NeuralNetworkNEAT from "machine-learning/NEAT/neuralNetworkNEAT";
 import NeuralNetwork from "machine-learning/neuralNetwork";
+import TargetSiteDealer from "mechanics/targetSiteDealer";
 import Vector2D from "utilities/vector2d";
 
 const appContainer: HTMLDivElement = document.getElementById("app") as HTMLDivElement;
@@ -16,6 +17,8 @@ const importBrainButton: HTMLInputElement = document.getElementById("button-impo
 
 let gameStopped = false;
 let brainType: string = "conventional";
+
+TargetSiteDealer.initialize(sites);
 
 importBrainButton.onchange = async (event: Event) => {
   let file = (<HTMLInputElement>event.target).files![0];
